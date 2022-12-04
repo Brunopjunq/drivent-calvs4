0bbf4f913120c6b3960020bdfe5a0108922846ab
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-12-04 13:11:18 -0300

    feat: Add test booking route GET /booking

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -20,3 +20,14 @@ export async function createRoomWithHotelId(hotelId: number) {
     }
   });
 }
+
+export async function createRoom(hotelId: number, capacity: number) {
+  return prisma.room.create({
+    data: {
+      name: faker.name.findName(),
+      capacity,
+      hotelId
+    }
+  });  
+}
+
